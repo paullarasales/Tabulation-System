9de363fec92ef0befbddef4c5eff8024fd9a8c38
Paul <ahmadzufukli@gmail.com>
2023-11-12 00:51:00 +0800

    finished login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,58 @@
 <x-guest-layout>
+    
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
 
-        <!-- Email Address -->
-        <div class="font-poppins text-gray-900 antialiased text-lg">
-            <x-input-label for="input_type" :value="__('Email/Username')" />
-            <x-text-input id="input_type" class="block mt-1 w-full" type="text" name="input_type" :value="old('input_type')" autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+    
+    <div class="h-3/6 flex flex-col items-center justify-center "> <!-- Add a custom class for styling if needed -->
+        <img src="{{ asset('images/psu_logo.png') }}" class="h-20 mb-4">
+        <h2 class="text-2xl font-bold mb-4">Login to Your Account</h2>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <!-- Email Address -->
+            <div class="font-poppins text-gray-900 antialiased text-lg">
+                <x-input-label for="input_type" :value="__('Email or Username')" />
+                <x-text-input id="input_type" class="block mt-1 w-96" type="text" name="input_type" :value="old('input_type')" autofocus autocomplete="username" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <x-input-error :messages="$errors->get('username')" class="mt-2" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            autocomplete="current-password" />
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('Password')" />
+                    <x-text-input id="password" class="block mt-1 w-96"
+                                  type="password"
+                                  name="password"
+                                  autocomplete="current-password" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" /> 
+                </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" /> 
+                <!-- Remember Me -->
+                <div class="block mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                        <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+
+                <div class="flex items-center justify-between mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+                    
+                    <x-primary-button class="ms-3">
+                        {{ __('Log in') }}
+                    </x-primary-button>
+                </div>
+            </form>
+
+            <!-- Registration Link -->
+            <div class="text-sm mt-4">
+                Don't have an account? <a href="{{ route('register') }}" class="text-blue-500">Register here</a>
+            </div>
         </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,25 +1,30 @@
 <x-guest-layout>
+
+
+    <div class="h-3/6 flex flex-col items-center justify-center ">
+    <img src="{{ asset('images/psu_logo.png') }}" class="h-20 mb-4">
+    <h2 class="text-2xl font-bold mb-4">Register your Account</h2>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"  autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-96" type="text" name="name" :value="old('name')"  autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
          <!-- Username -->
          <div class="mt-4">
             <x-input-label for="username" :value="__('Usename')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')"  autocomplete="username" />
+            <x-text-input id="username" class="block mt-1 w-96" type="text" name="username" :value="old('username')"  autocomplete="username" />
             <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"  autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-96" type="email" name="email" :value="old('email')"  autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -27,10 +32,10 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block mt-1 w-96"
                             type="password"
                             name="password"
-                            required autocomplete="new-password" />
+                            autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -39,7 +44,7 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+            <x-text-input id="password_confirmation" class="block mt-1 w-96"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
@@ -55,5 +60,6 @@
                 {{ __('Register') }}
             </x-primary-button>
         </div>
+    </div>
     </form>
 </x-guest-layout>
